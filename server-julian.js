0fb2bdf61b387598141db40f0ddc3c5a3fe55906
JSiow <julian.k.siow@gmail.com>
2019-10-15 19:41:20 -0700

    created post functionality using form

--- server-julian.js
+++ server-julian.js
@@ -1,14 +1,30 @@
+const onSuccess = ()=>{
+    console.log('success')
+}
+const onError = ()=>{
+    console.log('error')
+}
+
+const file = ()=>{
+    $.ajax({
+        method: 'POST',
+        url: 'http://localhost:3000/api/v1/new-subscriber',
+        data: {
+            "email": $('#email').val(),
+            "firstName": $('#firstName').val(),
+            "lastName": $('#lastName').val(),
+        },
+        success: onSuccess,
+        error: onError,
+    })
+}
 
 
+const handleSubscribe = function(event){
+    event.preventDefault();
+    file();
+}
 
-$ajax({
-    method: 'POST',
-    url: 'api/v1/subscribers',
-    data: {
-        email: $('#email').val(),
-        firstName: $('#firstName').val(),
-        lastName: $('#lastName').val()
-    },
-    success: onSuccess,
-    error: onError,
-})
+
+//----------------------Event Listener
+$('form').on('submit', handleSubscribe)
